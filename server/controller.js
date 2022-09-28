40fd4a73a8c86ff152d1efb2f6e64cff274dd17c
ChaseHilton <hilton.chase11@gmail.com>
2022-09-28 12:52:23 -0600

    getpostdelete

--- server/controller.js
+++ server/controller.js
@@ -1,7 +1,31 @@
 const LeaderBoard = require('./db.json');
+let NameId = 11;
 
 module.exports = {
   getLeaderBoard: (req, res) => {
+    res.status(200).send(LeaderBoard);
+  },
+
+  addName: (req, res) => {
+    const { name } = req.body;
+
+    let NewNameObject = {
+      id: NameId,
+      name: name,
+    };
+
+    LeaderBoard.push(NewNameObject);
+
+    NameId++;
+
+    res.status(200).send(LeaderBoard);
+  },
+
+  deleteName: (req, res) => {
+    const index = LeaderBoard.findIndex((el) => el.id === +req.params.id);
+
+    LeaderBoard.splice(index, 1);
+
     res.status(200).send(LeaderBoard);
   },
 };
--- server/index.js
+++ server/index.js
@@ -6,8 +6,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const { getLeaderBoard } = require('./controller');
+const { getLeaderBoard, addName, deleteName } = require('./controller');
 
 app.get('/getLeaderBoard', getLeaderBoard);
+
+app.post('/addName', addName);
+
+app.delete('/deleteName/:id', deleteName);
 
 app.listen(4000, () => console.log('server running on port 4000'));
